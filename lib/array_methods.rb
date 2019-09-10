fe36c3af225844aa522f2c72c950a55224751453
Matt Plichta <bigcatplichta@gmail.com>
2019-09-10 20:33:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,17 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  sorted_array = array.sort 
 end
 
 def using_reverse(array)
-
+  reversed_array = array.reverse 
 end
 
 def using_first(array)
-
+  array.first 
 end
 
 def using_last(array)
